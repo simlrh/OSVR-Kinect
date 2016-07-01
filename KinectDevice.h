2c50d4c707fa9c46025fb46c68641c579f601273
Steve Le Roy Harris <steve@nourish.je>
2016-07-01 16:57:23 +0100

    Initial commit

---
+++ KinectDevice.h
@@ -0,0 +1,47 @@
+#include <osvr/PluginKit/PluginKit.h>
+#include <osvr/PluginKit/TrackerInterfaceC.h>
+#include <osvr/PluginKit/AnalogInterfaceC.h>
+#include <osvr/PluginKit/ButtonInterfaceC.h>
+
+#include <Kinect.h>
+
+namespace KinectOsvr {
+	class KinectDevice {
+	public:
+		KinectDevice(OSVR_PluginRegContext ctx, IKinectSensor* pKinectSensor);
+		~KinectDevice();
+
+		OSVR_ReturnCode update();
+		static bool Detect(IKinectSensor** ppKinectSensor);
+	private:
+		void ProcessBody(IBody** ppBodies);
+		int addBody(int idx);
+		void removeBody(int idx);
+		bool firstBody(int channel);
+
+		osvr::pluginkit::DeviceToken m_dev;
+		OSVR_TrackerDeviceInterface m_tracker;
+		OSVR_AnalogDeviceInterface m_analog;
+		OSVR_ButtonDeviceInterface m_button;
+
+		IKinectSensor* m_pKinectSensor;
+		ICoordinateMapper*      m_pCoordinateMapper;
+		IBodyFrameReader*       m_pBodyFrameReader;
+
+		int m_channels[BODY_COUNT];
+
+		bool m_firstUpdate = true;
+		OSVR_PoseState m_offset;
+	};
+}
+
+// Safe release for interfaces
+template<class Interface>
+inline void SafeRelease(Interface *& pInterfaceToRelease)
+{
+	if (pInterfaceToRelease != NULL)
+	{
+		pInterfaceToRelease->Release();
+		pInterfaceToRelease = NULL;
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# OSVR-KinectV2
+
+An OSVR plugin providing Kinect SDK v2 position and orientation joint tracking, for use with a Kinect for Xbox One.
+
+git clone https://github.com/simlrh/OSVR-KinectV2
+cd OSVR-KinectV2
+git submodule init
+git submodule update
+
+Then follow the standard OSVR plugin build instructions.
